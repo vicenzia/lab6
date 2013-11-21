276247680862c67769ab2cdc73866696d0fb129a
vicenzia <vicenzia@ed441st7.engg.uregina.ca>
2013-11-21 08:48:36 -0600

    lab6

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author vicenzia
+ */
+public class JavaApplication1 {
+
+    int num;
+    static int maxNum;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JavaApplication1 obj1= new JavaApplication1();
+        obj1.num=1;
+        
+        
+        
+    }
+}
